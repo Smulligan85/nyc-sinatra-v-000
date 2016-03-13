0ad9163eaba79a82eeb8d084a8749ba64ee287c2
Smulligan85 <sean.mulligan85@gmail.com>
2016-03-13 09:15:18 -0700

    Done with lab

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311153837) do
+ActiveRecord::Schema.define(version: 20160313034933) do
 
   create_table "figure_titles", force: :cascade do |t|
     t.integer "title_id"
